a0f39155bfac494b8177f86104abef1508778191
showork620 <showork620>
2024-09-06 21:46:24 +0900

    fix: 不要アイテムを手動で選定した削除リストを作成・適用

--- html/wp-content/themes/lol_dictionary/functions/update_item_data.php
+++ html/wp-content/themes/lol_dictionary/functions/update_item_data.php
@@ -16,6 +16,24 @@ function getNewVersion() {
 $VERSION = getNewVersion();
 $URL = "https://ddragon.leagueoflegends.com/cdn/{$VERSION}/data/ja_JP/";
 
+// 使用不可、一覧に不要なアイテム
+$UNAVAILABLE_ITEMS = [
+    "1040", // オブシディアン エッジ
+    "126697", // ヒュプリス<br>
+    "1502", // 強化装甲
+    "1506", // 強化装甲
+    "2033", // コラプト ポーション
+    "2403", // ミニオン吸収装置
+    "3011", // ケミテック ピュートリファイアー
+    "3400", // お前の取り分
+    "4635", // リーチング リア
+    "4636", // ナイト ハーベスター
+    "4637", // 悪魔の抱擁
+    "4641", // スターリング ワードストーン
+    "6693", // プローラー クロウ
+    "8001" // アナセマ チェイン
+];
+
 // アイテムデータを取得
 function getOriginItemData($preUrl) {
 	$url = "{$preUrl}item.json";
@@ -28,8 +46,14 @@ $ITEMDATA = getOriginItemData($URL);
 
 // 不要なアイテムやプロパティを削除
 foreach ($ITEMDATA as $key => &$item) {
-	// チャンプ専用アイテムや説明がないアイテムを削除
-	if ($item['description'] === "" && isset($item['inStore']) || isset($item['requiredChampion'])) {
+
+	// 使用不可、一覧に不要なアイテムを削除
+	if ($item['description'] === "" && isset($item['inStore']) || in_array($key, $UNAVAILABLE_ITEMS)) {
+		unset($ITEMDATA[$key]);
+		continue;
+	}
+	// チャンピオン専用アイテム、aram・normalに未登場のアイテムを削除
+	if (! $item['maps']['11'] && ! $item['maps']['12'] || isset($item['requiredChampion'])) {
 		unset($ITEMDATA[$key]);
 		continue;
 	}
