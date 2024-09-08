f8d0fd9a1fedfee05ed7924cc49acf6db3dfef1e
showork620 <showork620>
2024-09-08 22:15:57 +0900

    feat: itemページのスタイル追加

--- html/wp-content/themes/lol_dictionary/page-item-dic.php
+++ html/wp-content/themes/lol_dictionary/page-item-dic.php
@@ -9,36 +9,38 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
-    <main id="main" class="site-main">
+		<main id="main" class="site-main">
 
-    <?php
-    // デバッグメッセージ
-    error_log('page-items.php template loaded');
+		<?php
+		// デバッグメッセージ
+		error_log('page-items.php template loaded');
 
-    // post_type が items の投稿を取得
-    $args = array(
-        'post_type' => 'items',
-        'posts_per_page' => -1,
-        'paged' => get_query_var('paged') ? get_query_var('paged') : 1
-    );
-    $the_query = new WP_Query($args);
+		// post_type が items の投稿を取得
+		$args = array(
+			'post_type' => 'items',
+			'posts_per_page' => -1,
+			'paged' => get_query_var('paged') ? get_query_var('paged') : 1
+		);
+		$the_query = new WP_Query($args);
 
-    if ($the_query->have_posts()) : ?>
-        <div class="items-thumbnails">
-            <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-                <div class="item-thumbnail">
-                    <?php if (has_post_thumbnail()) : ?>
-                        <?php the_post_thumbnail('thumbnail'); ?>
-                    <?php endif; ?>
-                </div>
-            <?php endwhile; ?>
-        </div>
-        <?php wp_reset_postdata(); ?>
-    <?php else : ?>
-        <p><?php _e('No items found.', 'text-domain'); ?></p>
-    <?php endif; ?>
+		if ($the_query->have_posts()) : ?>
+			<ul class="p-item-list">
+				<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+					<li class="p-item-list__item">
+						<?php if (has_post_thumbnail()) : ?>
+							<?php the_post_thumbnail('thumbnail'); ?>
+							<p class="id"><?php echo get_post_meta(get_the_ID(), 'id', true); ?></p>
+							<p class="name"><?php the_title(); ?></p>
+						<?php endif; ?>
+					</li>
+				<?php endwhile; ?>
+			</ul>
+			<?php wp_reset_postdata(); ?>
+		<?php else : ?>
+			<p><?php _e('No items found.', 'text-domain'); ?></p>
+		<?php endif; ?>
 
-    </main><!-- #main -->
+		</main><!-- #main -->
 </div><!-- #primary -->
 
 <?php
